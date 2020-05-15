2d242eb1f86cbabd4f9bb419c6d9885d3373bdf5
pbhadiyadra <pbhadiyadra@deakin.edu.au>
2020-05-15 16:11:34 +1000

    question one

---
+++ assessment 3.R
@@ -0,0 +1,21 @@
+# question one .
+#download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv",destfil="gene_expression.tsv")
+library(readr)
+library(dplyr)
+library(tidyr)
+#library(data.table)
+#library(tidyverse)
+library(tibble)
+
+url <- "https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv"
+#will read the data is namede as gene
+gene <- read_tsv(url)
+#name of the columns
+names(gene) <- c(" gene accession numbers", "SRR5150592", "SRR5150593")
+head(gene)
+str(gene)
+gene[1:6,1:3]
+
+
+
+

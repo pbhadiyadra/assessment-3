f6e3a4e1eeb4c0dd2a0d20913267078be36849c1
pbhadiyadra <pbhadiyadra@deakin.edu.au>
2020-05-24 12:36:56 +1000

    first half done

---
+++ assess-3.R
@@ -0,0 +1,53 @@
+
+# question one .
+#download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv",destfil="gene_expression.tsv")
+library(readr)
+library(dplyr)
+library(tidyr)
+#library(data.table)
+#library(tidyverse)
+library(tibble)
+
+url <- "https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv"
+#will read the data is named as gene
+gene <- read_tsv(url)
+str(gene)
+#name of the columns
+names(gene) <- c(" gene accession numbers", "SRR5150592", "SRR5150593")
+# gene accession number as row name
+gene <-read.table("gene_expression.tsv" , header = TRUE , row.names =  1)
+head(gene)
+str(gene)
+gene[1:6,1:2]
+
+#question 2
+
+
+#addition of col
+gene$Mean<-rowMeans(gene,na.rm = TRUE)
+# head of mean col
+head(gene$Mean)
+#head of whole gene data
+HEAD <-head(gene)
+
+#question 3
+
+
+#arranged in descending order
+Arranging <- gene%>%arrange(desc(Mean))%>% head(10)
+rownames(Arranging) <- rownames(gene) %>% head(10)
+Arranging
+
+#question 4
+
+#mean less than 10
+Mean[Mean < 10]
+# number of gene with mean less than 10 .
+Result <- Mean[Mean < 10]
+NROW(Result)
+
+#Question 5
+
+#histogram
+range(gene$Mean)
+histogram <- hist (gene$Mean,main = "Histogram of Mean Values",xlab = "Mean", ylab = "GAN" ,breaks = 10)
